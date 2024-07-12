d0f44bf8c7e28299e8e8205f5b91746cd0fc71b6
Dominic Nguyen <domynguyen_4@hotmail.com>
2024-07-11 17:55:51 -0700

    2407
    Addition of PACOTS plugin
    A222 G457 airways
    Updated chart supplement

---
+++ Plugins/dev2produkshun.bat
@@ -0,0 +1,5 @@
+COPY "C:\Users\domyn\source\repos\KZAK-PACOTS-SIGMETS\bin\Debug\PACOTSPlugin.dll" "C:\Users\domyn\OneDrive\Documents\GitHub\atop-oakland-dataset\Plugins"
+
+COPY "C:\Users\domyn\source\repos\vatsys-atop-plugin\bin\Debug\AtopPlugin.dll" "C:\Users\domyn\OneDrive\Documents\GitHub\atop-oakland-dataset\Plugins"
+
+pause
