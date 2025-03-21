8284d397b81f376e0c68b7170c558bcb69f11ae1
DivineWind04 <domynguyen_4@hotmail.com>
2025-03-21 10:13:40 -0700

    AIRAC 2503
    • Fixed some crashes associated with the AltitudeWindow
    • New functionality for the Send button within the AltitudeWindow to send via PM's if the aircraft is CPDLC equipped and toggleable HF to send to your freqeuency messages to emulate being sent to the radio operator and read out loud.
    • Probe button within the AltitudeWindow will now create a secondary flightplan to probe instead of the original. Hitting the Cancel button will remove that flightplan. You'll notice the callsign with * prepended. WIP
    • Overall stability improvements.

--- Plugins/dev2produkshun.bat
+++
@@ -1,7 +0,0 @@
-COPY "C:\Users\domyn\source\repos\KZAK-PACOTS-SIGMETS\bin\Debug\PACOTSPlugin.dll" "C:\Users\domyn\OneDrive\Documents\GitHub\atop-oakland-dataset\Plugins"
-
-COPY "C:\Users\domyn\source\repos\vatsys-atop-plugin\bin\Debug\AtopPlugin.dll" "C:\Users\domyn\OneDrive\Documents\GitHub\atop-oakland-dataset\Plugins"
-
-COPY "E:\vatsys\bin\Plugins\vatACARS.dll" "C:\Users\domyn\OneDrive\Documents\GitHub\atop-oakland-dataset\Plugins"
-
-pause
